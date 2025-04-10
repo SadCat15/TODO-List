276edea023854f3495644efebb87d37669dedef0
SadCat15 <michal.grzona777@gmail.com>
2025-04-10 21:39:10 +0200

    register endpoint

--- src/main/java/myprojects/todolist/controller/RestController.java
+++ src/main/java/myprojects/todolist/controller/RestController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import myprojects.todolist.exception.TaskException;
 import myprojects.todolist.model.Task;
 import myprojects.todolist.service.TaskService;
+import myprojects.todolist.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +14,31 @@ import org.springframework.web.bind.annotation.*;
 public class RestController {
 
     private final TaskService taskService;
+    private final UserService userService;
 
     @DeleteMapping("/delete{id}")
-    private ResponseEntity<String> deleteTask(@PathVariable Long id)
-    {
+    private ResponseEntity<String> deleteTask(@PathVariable Long id) {
         taskService.deleteTaskById(id);
         return ResponseEntity.status(200).body("Task deleted");
     }
 
     @PostMapping("/add-task")
-        private ResponseEntity<String> createTask(@RequestParam("name") String name, @RequestParam("description") String description) {
-        try{
-        taskService.saveTask(new Task(name, description));
-        return ResponseEntity.status(201).body("Task created");
-        } catch (TaskException e){
+    private ResponseEntity<String> createTask(@RequestParam("name") String name, @RequestParam("description") String description) {
+        try {
+            taskService.saveTask(new Task(name, description));
+            return ResponseEntity.status(201).body("Task created");
+        } catch (TaskException e) {
             return ResponseEntity.status(400).body(e.getUserMessage());
+        }
+    }
+
+    @PostMapping("/register-user")
+    private ResponseEntity<String> saveUser(@RequestParam("name") String name, @RequestParam("email") String email, @RequestParam("password") String password) {
+        try {
+            userService.registerUser(email, name, password);
+            return ResponseEntity.status(200).body("User registered");
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(e.getMessage());
         }
     }
 }
