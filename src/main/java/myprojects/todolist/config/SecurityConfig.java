d30eac8128824262da67adfba5bb952b35d20119
SadCat15 <michal.grzona777@gmail.com>
2025-04-17 18:46:00 +0200

    security config

--- src/main/java/myprojects/todolist/config/SecurityConfig.java
+++ src/main/java/myprojects/todolist/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package myprojects.todolist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,19 +21,28 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/register",
                                 "/css/**",
                                 "/js/**",
-                                "/api/**").permitAll()
-                        .requestMatchers("/").permitAll()
+                                "/api/**",
+                                "/login",
+                                "/registration")
+                        .permitAll()
+                        .requestMatchers("/tasks").hasAnyAuthority("USER")
                         .anyRequest().authenticated()
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(login -> login
                         .loginPage("/login")
+                        .defaultSuccessUrl("/tasks", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
