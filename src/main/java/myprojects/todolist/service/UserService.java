8b658fb9d1bfeaf0b82667e9109bd57a4bbb0465
SadCat15 <michal.grzona777@gmail.com>
2025-04-17 19:09:29 +0200

    created logger

--- src/main/java/myprojects/todolist/service/UserService.java
+++ src/main/java/myprojects/todolist/service/UserService.java
@@ -6,12 +6,15 @@ import myprojects.todolist.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final static Logger logger = Logger.getLogger("a");
 
     public void saveUser(User user) {
         userRepository.save(user);
@@ -26,6 +29,7 @@ public class UserService {
                 "USER"
                 );
         encodePassword(user);
+        logger.info(user.toString());
         saveUser(user);
     }
 
