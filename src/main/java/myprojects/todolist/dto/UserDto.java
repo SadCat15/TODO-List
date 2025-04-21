3544fe0a2f2acdee5a36f3253441c5514cdcb947
SadCat15 <michal.grzona777@gmail.com>
2025-04-21 12:35:21 +0200

    added not null validation

--- src/main/java/myprojects/todolist/dto/UserDto.java
+++ src/main/java/myprojects/todolist/dto/UserDto.java
@@ -1,9 +1,6 @@
 package myprojects.todolist.dto;
 
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,13 +12,17 @@ public class UserDto {
     @Null(message = "ID field should be null")
     private Long id;
     @NotBlank(message = "Name must be not emplty")
+    @NotNull(message = "Name must be not emplty")
     private String name;
     @NotBlank(message = "Emial must be not emplty")
+    @NotNull(message = "Emial must be not emplty")
     @Email(message = "Email should be valid")
     private String email;
     @NotBlank(message = "Password must be not emplty")
+    @NotNull(message = "Password must be not emplty")
     private String password;
-    @NotBlank(message = "Repeted password must be not emplty")
+    @NotBlank(message = "Repeated password must be not emplty")
+    @NotNull(message = "Repeated password must be not emplty")
     private String repetedPassword;
     @Null(message = "ROLE field should be null")
     private String role;
